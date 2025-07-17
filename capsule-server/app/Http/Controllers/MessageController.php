76f1ed24a3eec27344b024c700f8e2affdab9929
Sino57 <sinarfayyad5@gmail.com>
2025-07-18 01:26:34 +0300

    controllers

---
+++ capsule-server/app/Http/Controllers/MessageController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Message;
+
+class MessageController extends Controller{
+    
+    function getAllMessages(){
+        $messages = Message::all();
+
+        $response = [];
+        $response["status"] = "success";
+        $response["payload"] = $messages;
+
+        return json_encode($response, 200);
+    }
+
+    function addOrUpdateTask(Request $request, $id = null){
+        if($id){
+            $message = Message::find($id);
+        }else{
+            $message = new Message;
+        }
+        
+        $message->user_id = 0;
+        $message->title = $request["title"] || $task->title; 
+        $message->message =  $id && !isset($request["description"]) ?  $task->title : $request["description"];
+        $message->privacy = 0;
+        $message->color =  $id && !isset($request["color"]) ? $task->title : $request["color"];
+        $message->save();
+
+        $response = [];
+        $response["status"] = "success";
+        $response["payload"] = $message;
+
+        return json_encode($response, 200);
+    }
+
+}
