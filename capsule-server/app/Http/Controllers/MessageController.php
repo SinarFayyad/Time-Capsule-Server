59bf7aa565354f56de26d9bfcfc5fc97ab189ef9
Sino57 <sinarfayyad5@gmail.com>
2025-07-19 21:09:47 +0300

    controller

--- capsule-server/app/Http/Controllers/MessageController.php
+++ capsule-server/app/Http/Controllers/MessageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Services\MessageService;
 use Illuminate\Http\Request;
 
 use App\Models\Message;
@@ -11,15 +12,20 @@ use App\Traits\ResponseTrait;
 class MessageController extends Controller{
     
 
-    function getAllMessages(){
-        $messages = MessageService::getMessages();
+    function getMessages($id = null){
+        $messages = MessageService::getMessages($id);
         return $this->responseJSON($messages);
     }
 
-    function addMessage(Request $request, $id = null){
+    function addMessage(Request $request){
         
         $message = new Message;
         $message = MessageService::addMessage($request, $message);
+        return $this->responseJSON($message);
+    }
+
+    function deleteMessage (Request $request, $id){
+        $message = MessageService::deleteMessage($id);
         return $this->responseJSON($message);
     }
 
