c72127258d4e31c1be267d029d3782c95e68333a
Sino57 <sinarfayyad5@gmail.com>
2025-07-19 12:34:03 +0300

    Create AuthController

---
+++ capsule-server/app/Http/Controllers/Common/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Common;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use  App\Services\Common\AuthService;
+
+class AuthController extends Controller{
+
+    public function login(Request $request){
+        $user = AuthService::login($request);
+        if($user)
+            return $this->responseJSON($user);
+        return $this->responseJSON(null, "error", 401);
+    }
+
+    public function register(Request $request){
+        $user = AuthService::register($request);
+        return $this->responseJSON($user);
+    }
+}
