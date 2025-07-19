a9c32ce65945a1be6703179e19ce359274317ba6
Sino57 <sinarfayyad5@gmail.com>
2025-07-19 15:02:42 +0300

    editing routes

--- capsule-server/routes/api.php
+++ capsule-server/routes/api.php
@@ -6,13 +6,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\MessageController;
 
-Route::group(["prefix" => "user"], function(){
-    Route::get("/users", [UserController::class, "getUsers"]);
-    Route::post("/add_update_user/{id?}", [UserController::class, "addOrUpdateUser"]);
-});
 
-Route::group(["prefix" => "message"], function(){
-    Route::post("/add_update_message/{id?}", [MessageController::class, "addOrUpdateMessage"]);
-    Route::get("/messages", [MessageController::class, "getMessages"]);
-    Route::get("/remove_message", [MessageController::class, "removeMessages"]);
-});
+Route::get("/users", [UserController::class, "getUsers"]);
+Route::post("/add_update_user/{id?}", [UserController::class, "addOrUpdateUser"]);
+
+Route::post("/add_update_message/{id?}", [MessageController::class, "addOrUpdateMessage"]);
+Route::get("/messages", [MessageController::class, "getMessages"]);
+Route::get("/remove_message", [MessageController::class, "removeMessages"]);
+
