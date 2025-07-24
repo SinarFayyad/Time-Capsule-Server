8c2513fc965f029ce10ac7afebba268644300408
Sino57 <sinarfayyad5@gmail.com>
2025-07-24 21:08:08 +0300

    edit routes

--- capsule-server/routes/api.php
+++ capsule-server/routes/api.php
@@ -7,16 +7,19 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MessageController;
 
-Route::post("/login", [AuthController::class , "login"]);
-Route::post("/register", [AuthController::class , "register"]);
-Route::post('/logout', [AuthController::class, 'logout']);
 
-Route::get("/user/{id}", [UserController::class , "getUser"]);
-Route::post("/updateUser/{id}", [UserController::class, "UpdateUser"]);
+Route::group(["prefix" => "v0.1"], function(){
+    Route::post("/login", [AuthController::class , "login"]);
+    Route::post("/register", [AuthController::class , "register"]);
+    Route::post('/logout', [AuthController::class, 'logout']);
 
-Route::post("/addMessage", [MessageController::class, "addMessage"]);
-Route::get("/message/{id}", [MessageController::class, "getMessage"]);
-Route::get("/messages/{user_id?}", [MessageController::class, "getMessages"]);
-Route::get("/deleteMessage/{id}", [MessageController::class, "deleteMessage"]);
-Route::get("/getCapsules/{user_id}", [MessageController::class, "getCapsules"]); 
-// Route::post("/filterMessages", [MessageController::class , "filterMessages"]);
+    Route::get("/user/{id}", [UserController::class , "getUser"]);
+    Route::post("/updateUser/{id}", [UserController::class, "UpdateUser"]);
+
+    Route::post("/addMessage", [MessageController::class, "addMessage"]);
+    Route::get("/message/{id}", [MessageController::class, "getMessage"]);
+    Route::get("/messages/{user_id?}", [MessageController::class, "getMessages"]);
+    Route::get("/deleteMessage/{id}", [MessageController::class, "deleteMessage"]);
+    Route::get("/getCapsules/{user_id}", [MessageController::class, "getCapsules"]); 
+    // Route::post("/filterMessages", [MessageController::class , "filterMessages"]);
+});
